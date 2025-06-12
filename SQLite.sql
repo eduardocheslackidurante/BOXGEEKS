9d3ffd48dbc03874398a66213d1d733c3e418d6e
Matheushenriquedacruzdemoraes <159266966+Matheushenriquedacruzdemoraes@users.noreply.github.com>
2025-06-12 08:56:48 -0300

    SQL

---
+++ SQLite.sql
@@ -0,0 +1,10 @@
+INSERT INTO Clientes
+values
+(1,'Matheus','430.294.180-49','(43) 99840-2933','matheus@gmail.com'),
+(2,'Eduardo','980.026.790-59','(44) 97635-4813','eduardo@gmail.com'),
+(3,'Victor','226.909.960-57','(46) 99947-2874','victor@gmail.com'),
+(4,'Nicolas','494.778.910-70','(41) 99515-4366','nicolas@gmail.com'),
+(5,'Renan','403.707.060-05','(41) 99667-0437','renan@gmail.com');
+
+
+select * from Clientes
